8683128bf319fc248ebee879a6d740008612ef82
TapasCh <128989464+TapasCh@users.noreply.github.com>
2023-04-19 13:39:52 +0530

    Create Name.java

---
+++ Name.java
@@ -0,0 +1,7 @@
+public class Name {
+    public static void main(String[] args) {
+        System.out.println("Pratik Mukherjee, Roll No: 45");
+        for (String arg : args)
+            System.out.print(arg + " ");
+    }
+}
