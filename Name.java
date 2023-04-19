0f0f5a7795598ac40025d1f8a63dd030ddac0c3f
TapasCh <128989464+TapasCh@users.noreply.github.com>
2023-04-19 13:41:50 +0530

    Update Name.java

--- Name.java
+++ Name.java
@@ -1,6 +1,6 @@
 public class Name {
     public static void main(String[] args) {
-        System.out.println("Pratik Mukherjee, Roll No: 45");
+        System.out.println("Tapas Chatterjee, Roll No: 45");
         for (String arg : args)
             System.out.print(arg + " ");
     }
